606d75fcacd1809601eab8f5a6c2a5ca95c7c489
mohammadrezesk <mohammad.1392.sk@gmail.com>
2021-03-04 12:23:22 +0330

    final feature 6

--- Home/views.py
+++ Home/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, redirect
+from django.core.mail import send_mail
 
 
 # Create your views here.
@@ -20,6 +21,6 @@ def contactus(request):
             args["done"] = "done"
             # contact = ContactUs(title=title, email=email, text=text)
             # contact.save()
-            # TextWithEmail = text + '\n' + email
-            # send_mail(title, TextWithEmail, "asdsd@gmail.com", ['webe19lopers@gmail.com'], fail_silently=False, )
+            content = email + '\n' + text
+            send_mail(title, content, "asdsd@gmail.com", ['webe21lopers@gmail.com'], fail_silently=False, )
         return render(request, 'Home/contact.html', args)

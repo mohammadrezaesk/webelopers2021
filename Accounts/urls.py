62a69848ecc72b30eea6b6651495af20d42ba2b9
mohammadrezesk <mohammad.1392.sk@gmail.com>
2021-03-04 10:47:19 +0330

    final feature 2

--- Accounts/urls.py
+++ Accounts/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from Accounts import views
 urlpatterns = [
-    # path('register/', auth_views., name="register"),
+    path('register/', views.register, name="register"),
     path('login/', views.login, name="login"),
     path('logout/', views.logout, name="logout"),
 
--- Accounts/views.py
+++ Accounts/views.py
@@ -1,11 +1,27 @@
-from django.shortcuts import render
+from django.contrib.auth.models import User
 from django.shortcuts import render, redirect
 from django.contrib.auth import login as lgn, authenticate
 from django.contrib.auth import logout as lgt
 from django.contrib.auth.decorators import login_required
 
 
-# Create your views here.
+def register(request):
+    if request.method == "GET" and not request.user.is_authenticated:
+        return render(request, 'Accounts/register.html')
+    elif request.method == 'GET' and request.user.is_authenticated:
+        return redirect('/')
+    elif request.method == "POST":
+        firstname = request.POST['first_name']
+        lastname = request.POST['last_name']
+        username = request.POST['username']
+        email = request.POST['email']
+        password1 = request.POST['password1']
+        password2 = request.POST['password2']
+        user = User(first_name=firstname, last_name=lastname, username=username, email=email)
+        user.set_password(password1)
+        user.save()
+        return redirect('/')
+
 
 def login(request):
     if request.method == 'GET' and not request.user.is_authenticated:

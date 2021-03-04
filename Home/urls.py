145a6aa921cc0a2350311b29afe8d37651f62576
mohammadrezesk <mohammad.1392.sk@gmail.com>
2021-03-04 12:17:50 +0330

    final feature 5

--- Home/urls.py
+++ Home/urls.py
@@ -3,6 +3,7 @@ from django.urls import path, include
 from Home import views
 urlpatterns = [
     path('', views.homepage, name="homepage"),
+    path('contactus/', views.contactus, name="contactus"),
 
 
 ]
--- Home/views.py
+++ Home/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 
 # Create your views here.
@@ -6,3 +6,20 @@ from django.shortcuts import render
 def homepage(request):
     if request.method == "GET":
         return render(request, 'Home/homepage.html')
+
+
+def contactus(request):
+    args = {'done': ""}
+    if request.method == "GET":
+        return render(request, 'Home/contact.html', args)
+    else:
+        title = request.POST['title']
+        email = request.POST['email']
+        text = request.POST['text']
+        if 10 <= len(text) <= 250:
+            args["done"] = "done"
+            # contact = ContactUs(title=title, email=email, text=text)
+            # contact.save()
+            # TextWithEmail = text + '\n' + email
+            # send_mail(title, TextWithEmail, "asdsd@gmail.com", ['webe19lopers@gmail.com'], fail_silently=False, )
+        return render(request, 'Home/contact.html', args)

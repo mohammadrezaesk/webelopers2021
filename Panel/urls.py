de049edb3a863d750b928b3bc08b9ef440be9f56
mohammadrezesk <mohammad.1392.sk@gmail.com>
2021-03-04 12:47:25 +0330

    final feature 8

--- Panel/urls.py
+++ Panel/urls.py
@@ -2,6 +2,7 @@ from django.urls import path, include
 from Panel import views
 urlpatterns = [
     path('', views.panel, name="panel"),
+    path('new/', views.create_product, name="create_product"),
 
 
 ]
--- Panel/views.py
+++ Panel/views.py
@@ -4,3 +4,7 @@ from django.shortcuts import render
 # Create your views here.
 def panel(request):
     return render(request, "Panel/panel.html")
+
+
+def create_product(request):
+    return render(request, "Panel/create_product.html")

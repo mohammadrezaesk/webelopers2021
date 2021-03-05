21c8252275c68a32b9186dd627ced368707db781
intenvy.dev <intenvy.dev@yandex.com>
2021-03-05 09:40:26 +0330

    add tag model

--- Panel/models.py
+++ Panel/models.py
@@ -11,3 +11,10 @@ class Product(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class Tag(models.Model):
+    name = models.CharField(max_length=30)
+
+    def __str__(self):
+        return self.name
